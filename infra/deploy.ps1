a4fec90b06e169ae1d430e4def6b3f13f9eec19d
DanielInacio-code <dabernardo.inacio@gmail.com>
2026-04-28 17:17:37 +0100

    teste

--- infra/deploy.ps1
+++ infra/deploy.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 param(
-    [string]$Location = "italynorth",
+    [string]$Location = "switzerlandnorth",
     [string]$Prefix = "lastwrites",
     [string]$ResourceGroupName = "rg-lastwrites-5101021",
     [string]$GithubRepo = "",
