44b5e22abc46bb08a264d4c1f20c1de3a0105e70
davidvieira51756-code <davidvieira51756@gmail.com>
2026-04-29 09:38:18 +0100

    deploy fix

--- infra/deploy.ps1
+++ infra/deploy.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 param(
-    [string]$Location = "switzerlandnorth",
+    [string]$Location = "italynorth",
     [string]$Prefix = "lastwrites",
     [string]$ResourceGroupName = "rg-lastwrites-516251",
     [string]$GithubRepo = "",
@@ -155,6 +155,9 @@ function Get-AzCliRaw {
     )
 
     $output = Invoke-AzCli -Arguments $Arguments
+    if ($null -eq $output) {
+        return ""
+    }
     return Normalize-AzCliOutput -Output $output
 }
 
